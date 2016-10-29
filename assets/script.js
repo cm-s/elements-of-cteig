7976413b3d783a2e109ff8b4cda724497c95bbd8
cm-s <blufyr3530@gmail.com>
2016-10-29 00:20:29 -0700

    added search + nav menu & mechanics improvements

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,45 @@
 var numrand = 0;
 var navIsScrolled = false;
-var animated_last = {};
-var card_out = false;
+var card_out = { state: false, identifier: -1 };
 
 class seeker {
     constructor() {
         this.titles = [
             ['Leadership At All Levels', 0],
             ['High-Quality Curriculum And Instruction', 0],
-            ['Career Explorations And Guidance', 0],
-            ['Student Support and Student Leadership Development', 0],
-            ['Industry Partnerships', 0],
-            ['System Alignment and Coherence', 0],
-            ['Effective Organizational Design', 0],
-            ['System Responsiveness to Changing Economic Demands', 0],
-            ['Skilled Faculty and Professional Development', 0],
-            ['Evaluation, Accountability, and Continuous Improvement', 0],
-            ['CTE Promotion, Outreach, Marketing, and Communication', 0]
+            ['Career Explorations And Guidance', 265],
+            ['Student Support and Student Leadership Development', 477],
+            ['Industry Partnerships', 742],
+            ['System Alignment and Coherence', 954],
+            ['Effective Organizational Design', 1219],
+            ['System Responsiveness to Changing Economic Demands', 1484],
+            ['Skilled Faculty and Professional Development', 1749],
+            ['Evaluation, Accountability, and Continuous Improvement', 1961],
+            ['CTE Promotion, Outreach, Marketing, and Communication', 2028]
         ];
     };
-    // highlightor is the thing being highligted in the candidate
-    highlight_filter(candidate, highlightor) {
-        console.log(candidate.indexOf(highlightor));
-    };
     search(text) {
-        for (let i = 0; i < this.titles.length; i++) {
-            if (this.titles[i][0].indexOf(text) != -1) {
-                $('search-bar').append('<div class="serach-result">' + String(this.highlight_filter(this.titles[i][0], text)) + '</div>');
+        $('.search-result').remove();
+        if (text.length > 2) {
+            for (let index = 0; index < this.titles.length; index++) {
+                if (this.titles[index][0].toLowerCase().indexOf(text.toLowerCase()) != -1) {
+                    $('#search-bar').append($('<div class="search-result"></div>').text(String(this.titles[index][0])));
+                    $('.search-result:nth-last-of-type(1)').attr('id', index);
+                };
             };
         };
+        if ($('#search-bar').nextAll().prevObject[0].children.length > 3) {
+            $('#search-bar').css('border-bottom-left-radius', '0').css('border-bottom-right-radius', '0');
+        } else {
+            $('#search-bar').css('border-bottom-left-radius', '8px').css('border-bottom-right-radius', '8px');
+        };
+    };
+    close() {
+        this.search('\0\0');
+    };
+    direct_to(topic) {
+        $('html, body').animate({scrollTop: this.titles[topic][1]}, 300);
+        $('.search-result').remove();
     };
 };
 
@@ -36,7 +47,7 @@ function dorand() {
     // Removing animation class from old animated element
     $('.topic-wrapper:nth-child(' + numrand + ') .topic-box').removeClass('calling');
 
-    // Settings new random number
+    // Setting new random number
     numrand = Math.floor(Math.random() * 21);
 
     // Adding animation class to the random element
@@ -61,10 +72,10 @@ function animate_sweep(subject) {
         'left': '40%',
         'height': '96%',
         'width': '56vw',
-    }, 1000);
+    }, 700);
     setTimeout(function () {
         $(subject).css('box-shadow', 'inset 20px -12px 5px -12px rgba(0,0,0,0.3)');
-    }, 1100);
+    }, 800);
 };
 function animate_shrink(subject) {
     $(subject).css('box-shadow', 'none');
@@ -74,10 +85,10 @@ function animate_shrink(subject) {
         'margin': '30px auto 30px auto',
         'top': '0',
         'left': '0',
-    }, 1000);
+    }, 700);
     setTimeout(function () {
         clear_styles(subject);
-    }, 1001);
+    }, 800);
 };
 function animate_rise(subject) {
     $(subject).css('box-shadow', '0 1px 1px rgba(0,0,0,0.11)');
@@ -115,12 +126,16 @@ function animate_fall(subject) {
         }, 100);
     }, 50);
 }
+function feedback(container) {
+    console.log(container);
+};
 $(document).ready(function(event) {
     // Ensuring random elements animate
     dorand();
     // Activating search
     var searcher = new seeker();
 
+    // Animate navbar height changes
     $(document).on('scroll', function() {
         if (!navIsScrolled && (window.scrollY > 0)) {
             animate_rise('nav');
@@ -130,7 +145,37 @@ $(document).ready(function(event) {
             navIsScrolled = false;
         };
     });
+
+    $('#dot-menu-container').click(function(event) {
+        $('#dot-menu-container .ripple').css('top', (event.pageY - 80) - $(this).offset().top + 'px')
+                                        .css('left', (event.pageX - 80) - $(this).offset().left + 'px');
+
+        $('#dot-menu-container .ripple').addClass('active').delay(400).queue(function() {
+            $('#dot-menu-container .ripple').removeClass('active').dequeue();
+        });
+        setTimeout(function () {
+            $('#dot-menu').addClass('active');
+            $('#dot-menu-container').addClass('active');
+        }, 200);
+        $('#dot-menu li').click(function() {
+            feedback(this);
+        });
+        $('#view-pdf-link').click(function() {
+            let new_window = window.open('https://goo.gl/0QEaaa', '_blank');
+            new_window.focus();
+        });
+        /*
+
+        $('html').click(function() {
+        $('#dot-menu-container').removeClass('active');
+        $('#dot-menu').removeClass('active');
+    })
+        */
+    })
+
+    // Control search bar functionality mechanics
     $('#search-bar').click(function(event) {
+        searcher.search($('#search-field').val());
         $('#search-field').keyup(function() {
             searcher.search($(this).val());
         });
@@ -138,24 +183,41 @@ $(document).ready(function(event) {
         event.stopPropagation();
         $('html').click(function() {
             $('#search-bar').removeClass('active');
+            searcher.close();
+        });
+        $('.search-result').click(function(event) {
+            searcher.direct_to(Number($(this).attr('id')));
         });
     });
-    $('')
 
-    // Add and remove 'pressed' class from clicked .topic-box(es)
+    // Control topic box viewing and animation invocation mechanics
     $('.topic-box').click(function() {
-        if (!card_out) {
+        if (!card_out.state) {
             $(this).removeClass('reset').addClass('active');
             animate_sweep(this);
             $('body').addClass('no_scroll');
-            card_out = true;
+            card_out.state = true;
+            card_out.identifier = $(this).attr('id');
+        } else {
+            if ($(this).attr('id') != card_out.identifier) {
+                $('body').addClass('no_scroll');
+                $('.topic-box#' + card_out.identifier).removeClass('active').addClass('reset');
+                animate_shrink('.topic-box#' + card_out.identifier);
+                let this_card = $(this);
+                setTimeout(function () {
+                    $(this_card).removeClass('reset').addClass('active');
+                    animate_sweep(this_card);
+                    card_out.state = true;
+                    card_out.identifier = $(this_card).attr('id');
+                }, 1100);
+            };
         };
     });
     $('button').click(function (event) {
         $('.topic-box').removeClass('active').addClass('reset');
         animate_shrink('.topic-box');
         $('body').removeClass('no_scroll');
-        card_out = false;
+        card_out.state = false;
         event.stopPropagation();
     });
 });
