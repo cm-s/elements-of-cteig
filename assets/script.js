586817fe7a55d570b7d9f9aee7c41e2c7959aca6
cm-s <blufyr3530@gmail.com>
2016-11-06 19:04:27 -0800

    upgraded search functions

--- assets/script.js
+++ assets/script.js
@@ -6,25 +6,38 @@ var menu_out = { state: false };
 class seeker {
     constructor() {
         this.titles = [
-            ['Leadership At All Levels', 0],
-            ['High-Quality Curriculum And Instruction', 0],
-            ['Career Explorations And Guidance', 265],
-            ['Student Support and Student Leadership Development', 477],
-            ['Industry Partnerships', 742],
-            ['System Alignment and Coherence', 954],
-            ['Effective Organizational Design', 1219],
-            ['System Responsiveness to Changing Economic Demands', 1484],
-            ['Skilled Faculty and Professional Development', 1749],
-            ['Evaluation, Accountability, and Continuous Improvement', 1961],
-            ['CTE Promotion, Outreach, Marketing, and Communication', 2028]
+            ['Element', 'Leadership At All Levels', 0],
+            ['Element', 'High-Quality Curriculum And Instruction', 0],
+            ['Element', 'Career Explorations And Guidance', 265],
+            ['Element', 'Student Support and Student Leadership Development', 477],
+            ['Element', 'Industry Partnerships', 742],
+            ['Element', 'System Alignment and Coherence', 954],
+            ['Element', 'Effective Organizational Design', 1219],
+            ['Element', 'System Responsiveness to Changing Economic Demands', 1484],
+            ['Element', 'Skilled Faculty and Professional Development', 1749],
+            ['Element', 'Evaluation, Accountability, and Continuous Improvement', 1961],
+            ['Element', 'CTE Promotion, Outreach, Marketing, and Communication', 2028],
+            ['Req', 'The CTE pathways are articulated with post-secondary and industry through programs of study, formal articulation agreements, and/or Tech Prep.', 1],
+            ['Req', 'Local district administrators participate in CTE professional development regarding the benefits of CTE and the management of CTE within the larger context of educational improvement to serve all students.', 1],
+            ['Req', 'Investment is made to provide support for CTE leadership at the local level to ensure that CTE administrators, teacher(s), and counseling and instructional leaders have sufficient time and resources to implement system improvements and work with their counterparts in other programs.', 1],
+            ['Req', 'The CTE Model Curriculum Standards and Framework for the __________________ Industry Sector are the basis for content of courses offered. Curriculum addresses "Pathway" standards within the program pathway(s) and course sequence.', 2],
+            ['Req', 'Career paths have been identified and can be found on a chart or diagram in the CTE Plan.', 2],
+            ['Req', 'The CTE program has classroom-linked work-based learning and work experience education opportunities through strengthened industry partnerships, effective coordination with Regional Occupation Center/Program (ROC/P), adult schools, Work Experience Education, and Cooperative Work Experience Education programs, and a systematic review of policies and practices addressing barriers to access, including insurance, liability, and other issues.', 2],
+            ['Req', 'The school master schedule allows students to follow the recommended sequence of CTE courses to complete the selected career path(s).', 2],
+            ['Req', 'Students are provided with a strong experience in and understanding of all aspects of industry.', 2],
+            ['Req', 'Technology is incorporated into program instruction.', 2],
+            ['Req', 'There is collaboration between academic and CTE teachers.', 2],
+            ['Req', 'CTE courses are industry certified, have been submitted to meet high school graduation requirements, University of California a-g (UC a-g) credit, or articulated with a community college.', 2]
+
+
         ];
     };
     search(text) {
         $('.search-result').remove();
         if (text.length > 2) {
             for (let index = 0; index < this.titles.length; index++) {
-                if (this.titles[index][0].toLowerCase().indexOf(text.toLowerCase()) != -1) {
-                    $('#search-bar').append($('<div class="search-result"></div>').text(String(this.titles[index][0])));
+                if (this.titles[index][1].toLowerCase().indexOf(text.toLowerCase()) != -1) {
+                    $('#search-bar').append($('<div class="search-result"></div>').text(this.filter(String(this.titles[index][1]), this.strip(text.toLowerCase()))));
                     $('.search-result:nth-last-of-type(1)').attr('id', index);
                 };
             };
@@ -35,11 +48,61 @@ class seeker {
             $('#search-bar').css('border-bottom-left-radius', '8px').css('border-bottom-right-radius', '8px');
         };
     };
+    strip(text) {
+        let output = ''
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] != ' ') {
+                output += text[i];
+            };
+        };
+        return output;
+    };
+    filter(result, search) {
+        let purified_result = result.toLowerCase()
+        let output = '';
+        let place = 0;
+        while (true) {
+            let word = '';
+            for (let index = place; index < purified_result.length; index++) {
+                if (purified_result[index] == ' ') {
+                    place = index + 1;
+                    break;
+                };
+                word += purified_result[index];
+            };
+            if (word.indexOf(search) != -1) {
+                break;
+            };
+            if ((purified_result.length - place) <= 5) {
+                break;
+            };
+        };
+        if (result.length > 62) {
+            if (place > 31 && (result.length - place) + place < result.length) {
+                output += '.';
+                output += '.';
+                output += '.';
+                for (let index = place - 31; index < (result.length - (result.length - place) + 20); index++) {
+                    output += result[index];
+                };
+            } else {
+                for (let index = 0; index < 62; index++) {
+                    output += result[index];
+                };
+            };
+            output += '.';
+            output += '.';
+            output += '.';
+        } else {
+            output = result;
+        };
+        return output;
+    };
     close() {
         this.search('\0\0');
     };
     direct_to(topic) {
-        $('html, body').animate({scrollTop: this.titles[topic][1]}, 300);
+        $('html, body').animate({scrollTop: this.titles[topic][2]}, 300);
         $('.search-result').remove();
     };
 };
